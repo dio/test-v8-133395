319e8228f42b9e8d3e3b9f2ed36f2ac46c84bc73
Dhi Aurrahman <dio@rockybars.com>
2022-11-10 21:47:09 +0700

    Initial commit
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# [v8 id=133395](https://bugs.chromium.org/p/v8/issues/detail?id=13395)
+
+## WasmEngines/TestVm.WasmMemoryLimit/v8
+
+On macOS with `bazel`/`bazelisk` installed run:
+
+```console
+./test.sh
+```
+
+To install `bazelisk`:
+
+```console
+brew install bazelisk
+```
---
+++ test.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clone the repo.
+rm -fr proxy-wasm-cpp-host
+git clone https://github.com/proxy-wasm/proxy-wasm-cpp-host.git
+
+# Set working directory.
+cd proxy-wasm-cpp-host
+
+# Build test data.
+bazel build --verbose_failures --test_output=errors --config=clang -c opt $(bazel query 'kind(was.*_rust_binary, //test/test_data/...)') $(bazel query 'kind(_optimized_wasm_cc_binary, //test/test_data/...)')
+
+# Clean up test data.
+for i in $(find bazel-bin/test/test_data/ -mindepth 1 -maxdepth 1 -type d); do \
+  rm -rf $i; \
+done
+
+# Copy test data.
+cp -fr bazel-bin/test/test_data/* test/test_data/
+
+# Mangle build rules to use existing test data.
+sed 's/\.wasm//g' test/BUILD > test/BUILD.tmp && mv test/BUILD.tmp test/BUILD
+echo "package(default_visibility = [\"//visibility:public\"])" > test/test_data/BUILD
+for i in $(cd test/test_data && ls -1 *.wasm | sed 's/\.wasm$//g'); do \
+  echo "filegroup(name = \"$i\", srcs = [\"$i.wasm\"])" >> test/test_data/BUILD; \
+done
+
+# Run the TestVm.WasmMemoryLimit 100 times. Set "-Wno-deprecated-declarations" since:
+# external/v8/src/base/platform/platform-darwin.cc:56:22: error: 'getsectdatafromheader_64' is
+# deprecated: first deprecated in macOS 13.0 [-Werror,-Wdeprecated-declarations].
+bazel test \
+  --cxxopt=-Wno-deprecated-declarations \
+  --host_cxxopt=-Wno-deprecated-declarations \
+  --verbose_failures \
+  --test_output=errors \
+  --define engine=v8 \
+  --test_arg=--gtest_filter=TestVm.WasmMemoryLimit \
+  --test_arg=--gtest_repeat=100 \
+  //test:runtime_test
